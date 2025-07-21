cb2485184e570f8a6c3c89ec380146f30eb9da95
ayush <ayush.mehta010906@gmail.com>
2025-07-21 10:54:08 +0000

    completed the project

--- rock-paper-scissors/01.js
+++ rock-paper-scissors/01.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice(){
   let randomNumber = Math.random();
   if(randomNumber < (1/3)){
@@ -9,3 +13,63 @@ function getComputerChoice(){
   }
 }
 
+function getHumanChoice(){
+  let humanChoice = prompt("Choose an option");
+  return humanChoice;
+}
+
+function displayScore(){
+  console.log(`Score => You: ${humanScore}, Computer: ${computerScore}`);
+}
+
+function playRound(computerChoice, humanChoice){
+  computerChoice = getComputerChoice();
+  humanChoice = getHumanChoice();
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === 'rock'){
+    if(computerChoice === 'Rock'){
+      console.log ("It's a tie!");
+      displayScore();
+    }else if(computerChoice === 'Paper'){
+      console.log("You lose!");
+      computerScore ++;
+      displayScore();
+    }else{
+      console.log("You win!")
+      humanScore++;
+      displayScore();
+    }
+  }
+  if (humanChoice === 'paper'){
+    if(computerChoice === 'Paper'){
+      console.log ("It's a tie!");
+      displayScore();
+    }else if(computerChoice === 'Scissors'){
+      console.log("You lose!");
+      computerScore ++;
+      displayScore();
+    }else{
+      console.log("You win!")
+      humanScore++;
+      displayScore();
+    }
+  }
+  if (humanChoice === 'scissors'){
+    if(computerChoice === 'Scissors'){
+      console.log ("It's a tie!");
+      displayScore();
+    }else if(computerChoice === 'Rock'){
+      console.log("You lose!");
+      computerScore ++;
+      displayScore();
+    }else{
+      console.log("You win!")
+      humanScore++;
+      displayScore();
+    }
+  }
+}
+for (let i = 0; i < 5; i++){
+  playRound();
+}
